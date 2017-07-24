b6d1cbaf3e50459ac7cd877330bbe8cea0270951
patrickreillynoland <patrickreillynoland@gmail.com>
2017-07-24 17:27:38 -0400

    Deleted extraneous middleware folder, upped max connects to db

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-    connectionLimit: 100,
+    connectionLimit: 1000,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
