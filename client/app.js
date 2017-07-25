41b7dee103911e8312098e841a50ff6c11a627a3
patrickreillynoland <patrickreillynoland@gmail.com>
2017-07-25 11:53:06 -0400

    App.js routing for login and users

--- client/app.js
+++ client/app.js
@@ -1,13 +1,17 @@
-angular.module('Coworkers', ['ngRoute', 'ngResource', 'Coworkers.controllers', 'Coworkers.factories', 'Coworkers.services'])
+angular.module('Coworkers'['ngRoute', 'ngResource', 'Coworkers.controllers', 'Coworkers.factories'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
     .when('/', {
         templateUrl: 'views/welcome.html'
     })
-    .when('/workers/:id', {
-        templateUrl: 'views/workers.html',
-        controller: 'WorkersController'
+    .when('/login', {
+        templateUrl: 'views/category.html',
+        controller: 'CategoryController'
+    })
+    .when('/users', {
+        templateUrl: 'views/users.html',
+        controller: 'UsersController'
     })
     .otherwise({
         redirectTo: '/'
