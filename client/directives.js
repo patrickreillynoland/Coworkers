0c25b1725ad048e81be8b8208b848e8d997ac55f
patrickreillynoland <patrickreillynoland@gmail.com>
2017-08-10 15:26:58 -0400

    Now loads placeholder image before real image, instead of red square

--- client/directives.js
+++ client/directives.js
@@ -134,4 +134,15 @@ angular.module('Coworkers.directives', [])
             });
         }
     };
-}]);
+}])
+.directive('hires', function() {
+  return {
+    restrict: 'A',
+    scope: { hires: '@' },
+    link: function(scope, element, attrs) {
+        element.one('load', function() {
+            element.attr('src', scope.hires);
+        });
+    }
+  };
+});
